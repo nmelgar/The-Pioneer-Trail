2c1a7ba260d8ca03815c8f607ba713c138429cec
jnrpobee <jnr.pobee@gmail.com>
2018-05-21 23:49:58 +0100

    class of resources committing.

---
+++ PioneerTrail/src/byui/cit260/PioneerTrail/model/Resources.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.PioneerTrail.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Pobee
+ */
+public class Resources implements Serializable{
+    
+    private String name;
+    private Float energyValue;
+    private int weight;
+    private String type;
+
+    public Resources(String name, Float energyValue, int weight, String type) {
+        
+        this.name = name;
+        this.energyValue = energyValue;
+        this.weight = weight;
+        this.type = type;
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getEnergyValue() {
+        return energyValue;
+    }
+
+    public void setEnergyValue(Float energyValue) {
+        this.energyValue = energyValue;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + Objects.hashCode(this.energyValue);
+        hash = 89 * hash + Objects.hashCode(this.weight);
+        hash = 89 * hash + Objects.hashCode(this.type);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Resources other = (Resources) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        if (!Objects.equals(this.energyValue, other.energyValue)) {
+            return false;
+        }
+        if (!Objects.equals(this.weight, other.weight)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Resources{" + "name=" + name + ", energyValue=" + energyValue + ", weight=" + weight + ", type=" + type + '}';
+    }
+            
+    
+    
+}
