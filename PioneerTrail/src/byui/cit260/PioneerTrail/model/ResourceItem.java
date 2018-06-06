f1946bb2cf0e86db48df2e057a65834050902ad8
jnrpobee <jnr.pobee@gmail.com>
2018-06-06 05:12:02 +0100

    class of resources committing.

---
+++ PioneerTrail/src/byui/cit260/PioneerTrail/model/ResourceItem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.PioneerTrail.model;
+
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author po
+ */
+public class ResourceItem implements Serializable{
+
+        private double weight;
+
+    public ResourceItem(double weight) {
+        this.weight = weight;
+    }
+
+      
+    
+}
