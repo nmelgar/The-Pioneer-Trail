81249a0ed1953e829db714ce2bb14b72b35cda0a
jnrpobee <jnr.pobee@gmail.com>
2018-06-06 07:01:46 +0100

    class of resourceItem committing.

---
+++ PioneerTrail/src/byui/cit260/PioneerTrail/model/Toolbox.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.PioneerTrail.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author po
+ */
+public class Toolbox implements Serializable{
+    
+    private final int volume;
+
+    public Toolbox(int volume) {
+        this.volume = volume;
+    }
+    
+    
+    
+}
