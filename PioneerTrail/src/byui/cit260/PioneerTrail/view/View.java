05c8ec631dead8296ce9d2e5e537bd05d2bd3b18
nmelgar <nmelgar16013@gmail.com>
2018-06-22 20:25:37 -0500

    June, 22 - 18 Week 9 Team Assignment unfinished

---
+++ PioneerTrail/src/byui/cit260/PioneerTrail/view/View.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.PioneerTrail.view;
+
+
+/**
+ *
+ * @author MMG
+ */
+public abstract class View implements ViewInterface {
+    
+    public View(){
+        
+    }
+}
