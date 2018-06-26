e4c0105e10bb07ee51647d8842f77a9aa4d05460
jnrpobee <jnr.pobee@gmail.com>
2018-06-26 01:47:58 +0100

    new updates on ResumeMenuView

---
+++ PioneerTrail/src/byui/cit260/PioneerTrail/view/UsingResourcesView.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.PioneerTrail.view;
+
+/**
+ *
+ * @author po
+ */
+public class UsingResourcesView extend view{
+
+ public UsingResourcesView(){
+
+super(
+
+)
+}
+    
+}
